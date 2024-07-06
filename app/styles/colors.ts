0e9de8311a43538731d4ca8a2280f46eeee26365
hasn-ab <hasn.abbas94@gmail.com>
2024-07-06 15:23:58 +0800

    task(ui): add search bar with button

---
+++ app/styles/colors.ts
@@ -0,0 +1,5 @@
+export const Colors = {
+  border: '#E2E2E2',
+};
+
+Object.freeze(Colors);
--- app/views/HomeScreen.tsx
+++ app/views/HomeScreen.tsx
@@ -1,18 +1,48 @@
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
+import {Colors} from '../styles/colors';
 
 export default function HomeScreen() {
   const [inputValue, setInputValue] = React.useState('');
 
-  return (
-    <View style={styles.container}>
+  const renderLoading = () => {};
+
+  const searchNews = () => {};
+
+  const renderSearchBar = () => {
+    return (
       <View style={styles.searchInputContainer}>
         <TextInput
           value={inputValue}
           onChangeText={setInputValue}
           style={styles.input}
         />
+        <TouchableOpacity onPress={searchNews}>
+          <Image
+            source={require('../assets/search.png')}
+            style={{width: 25, height: 25}}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
       </View>
+    );
+  };
+  const renderNewsFeed = () => {
+    return null;
+  };
+
+  return (
+    <View style={styles.container}>
+      {renderSearchBar()}
+      {renderNewsFeed()}
     </View>
   );
 }
@@ -26,11 +56,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     marginHorizontal: 10,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+    borderWidth: 1,
     marginTop: 10,
+    borderColor: Colors.border,
+    overflow: 'hidden',
   },
   input: {
     height: 40,
-    margin: 12,
-    borderWidth: 1,
+    width: '80%',
+  },
+  searchIconContainer: {
+    backgroundColor: 'grey',
   },
 });
