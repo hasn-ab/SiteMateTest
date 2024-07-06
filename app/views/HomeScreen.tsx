a07795f7e8205e8967265aaea5b8d82c4d5f7c62
hasn-ab <hasn.abbas94@gmail.com>
2024-07-06 16:30:03 +0800

    task(core): connect api with UI

--- app/views/HomeScreen.tsx
+++ app/views/HomeScreen.tsx
@@ -1,4 +1,5 @@
 import {
+  FlatList,
   Image,
   StyleSheet,
   Text,
@@ -7,15 +8,62 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Colors} from '../styles/colors';
+import {getNews} from '../apis/newsApi';
+import {useQuery} from 'react-query';
+import NewsItem from './components/NewsItem';
+
+interface FeedProps {
+  query: string;
+}
+
+const Feed = ({query}: FeedProps) => {
+  const {isLoading, error, data, refetch} = useQuery(
+    ['newsData', query],
+    async () => {
+      const data = await getNews(query);
+      return data;
+    },
+  );
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
+  if (isLoading) return <Text style={styles.loadingText}>Loading...</Text>;
+  if (error) return <Text>An error has occurred: {error.message}</Text>;
+
+  if (!data) {
+    return null;
+  }
+  return (
+    <View style={styles.listContainer}>
+      <FlatList
+        data={data.articles}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => (
+          <NewsItem
+            title={item.title}
+            description={item.description}
+            date={item.publishedAt}
+            imageUri={item.urlToImage}
+          />
+        )}
+        keyExtractor={item => item.title}
+      />
+    </View>
+  );
+};
+
+const MemoizedFeed = React.memo(Feed);
 
 export default function HomeScreen() {
   const [inputValue, setInputValue] = React.useState('');
+  const [query, setQuery] = React.useState('');
 
-  const renderLoading = () => {};
-
-  const searchNews = () => {};
+  const searchNews = async () => {
+    setQuery(inputValue);
+  };
 
   const renderSearchBar = () => {
     return (
@@ -24,8 +72,9 @@ export default function HomeScreen() {
           value={inputValue}
           onChangeText={setInputValue}
           style={styles.input}
+          testID="search-input"
         />
-        <TouchableOpacity onPress={searchNews}>
+        <TouchableOpacity onPress={searchNews} testID="search-button">
           <Image
             source={require('../assets/search.png')}
             style={{width: 25, height: 25}}
@@ -36,7 +85,7 @@ export default function HomeScreen() {
     );
   };
   const renderNewsFeed = () => {
-    return null;
+    return <MemoizedFeed query={query} />;
   };
 
   return (
@@ -69,5 +118,12 @@ const styles = StyleSheet.create({
   },
   searchIconContainer: {
     backgroundColor: 'grey',
+  },
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  loadingText: {
+    alignSelf: 'center',
   },
 });
